e8d76c79b2129fd8deeb184472f463f4f512c8f4
AngeloMidolo97 <angelomidolo97@gmail.com>
2022-11-29 17:35:49 +0100

    add ngif

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/post.interface';
+import * as postSrv from '../../services/post.service'
 
 @Component({
   selector: 'app-home',
@@ -6,8 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  post: Post[] = [];
+  constructor() {
+    postSrv.getPost().then(post => this.post = post)
+  }
 
   ngOnInit(): void {
   }
